fe325c28a0b25a3129113da0e135ecfad5fa22f9
Jose Nascimento <joseeantonioo2000@gmail.com>
2025-03-30 09:44:17 -0300

    feat: correction of layout

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,13 +3,7 @@
 
 import type React from "react"
 
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </NextThemesProvider>
-  )
+  return <>{children}</>
 }
 
